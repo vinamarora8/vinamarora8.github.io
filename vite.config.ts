92444e434121034f1b9ceb5763cf49543cfca12b
Vinam Arora <vinam@posteo.de>
2025-03-14 23:12:43 -0400

    Update for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // For user site (username.github.io), we don't need a base path
+  base: '/',
 })
