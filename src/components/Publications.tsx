16e3ddd69514abda2aab4595a30a9e4eac56eba7
Vinam Arora <vinam@posteo.de>
2025-03-15 19:12:50 -0400

    Cleanup publications component

--- src/components/Publications.tsx
+++ src/components/Publications.tsx
@@ -38,10 +38,7 @@ const Publications: React.FC = () => {
         {publicationList.map((publication, index) => (
           <Accordian key={index}>
             {({ isExpanded }) => (
-              <PublicationContent 
-                expanded={isExpanded}
-                publication={publication}
-              />
+              <PublicationContent expanded={isExpanded} publication={publication} />
             )}
           </Accordian>
         ))}
@@ -50,15 +47,14 @@ const Publications: React.FC = () => {
   );
 };
 
-
 interface PublicationContentProps {
   expanded: boolean;
   publication: Publication;
 }
 
 const PublicationContent: React.FC<PublicationContentProps> = ({ expanded, publication: pub }) => {
-    return (
-      <div className="publications__content">
+  return (
+    <div className="publications__content">
       <h3 className="publications__title">{pub.title}</h3>
       <div className="publications__authors">
         {pub.authors.map((author, index) => (
@@ -68,55 +64,48 @@ const PublicationContent: React.FC<PublicationContentProps> = ({ expanded, publi
           </React.Fragment>
         ))}
       </div>
-      <div className="publications__details">
-        <div className="publications__details__venue">
-          {pub.venue}, {pub.year}
-        </div>
+      <div className="publications__venue">
+        {pub.venue}, {pub.year}
+      </div>
 
-        <div
-          className={`publications__details__abstract ${
-            expanded ? `publications__details__abstract--visible` : ''
-          }`}
-        >
-          <div style={{ height: 15 }}></div>
-          <span style={{ fontWeight: 400 }}>Abstract: </span>
-          {pub.abstract}
-          <div style={{ height: 15 }}></div>
-        </div>
+      <div
+        className={`publications__abstract ${
+          expanded ? `publications__abstract--visible` : ''
+        }`}
+      >
+        <div style={{ height: 15 }}></div>
+        <span style={{ fontWeight: 400 }}>Abstract: </span>
+        {pub.abstract}
+        <div style={{ height: 15 }}></div>
+      </div>
 
-        <div className="publications__details__links">
-          {pub.links.paper && (
-            <a href={pub.links.paper} target="_blank" rel="noopener noreferrer">
-              <FaFilePdf /> <p>Paper</p>
-            </a>
-          )}
+      <div className="publications__links">
+        {pub.links.paper && (
+          <a href={pub.links.paper} target="_blank" rel="noopener noreferrer">
+            <FaFilePdf /> <p>Paper</p>
+          </a>
+        )}
 
-          {pub.links.project && (
-            <a href={pub.links.project} target="_blank" rel="noopener noreferrer">
-              <FaLink /> <p>Project Page</p>
-            </a>
-          )}
+        {pub.links.project && (
+          <a href={pub.links.project} target="_blank" rel="noopener noreferrer">
+            <FaLink /> <p>Project Page</p>
+          </a>
+        )}
 
-          {pub.links.poster && (
-            <a href={pub.links.poster} target="_blank" rel="noopener noreferrer">
-              <FaImage /> <p>Poster</p>
-            </a>
-          )}
+        {pub.links.poster && (
+          <a href={pub.links.poster} target="_blank" rel="noopener noreferrer">
+            <FaImage /> <p>Poster</p>
+          </a>
+        )}
 
-          {pub.links.openReview && (
-            <a
-              href={pub.links.openReview}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaLink /> <p>OpenReview</p>
-            </a>
-          )}
-        </div>
+        {pub.links.openReview && (
+          <a href={pub.links.openReview} target="_blank" rel="noopener noreferrer">
+            <FaLink /> <p>OpenReview</p>
+          </a>
+        )}
       </div>
     </div>
-    )
-
-}
+  );
+};
 
 export default Publications;
