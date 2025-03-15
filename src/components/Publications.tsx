efb0135b2e22fb81eee88ab839e76927793f74b1
Vinam Arora <vinam@posteo.de>
2025-03-14 23:06:34 -0400

    Cleanup publications

--- src/components/Publications.tsx
+++ src/components/Publications.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFilePdf, FaGithub, FaImage, FaExternalLinkAlt } from 'react-icons/fa';
+import { FaFilePdf, FaImage, FaExternalLinkAlt } from 'react-icons/fa';
 
 interface Publication {
   id: number;
@@ -19,8 +19,8 @@ const publicationsData: Publication[] = [
   {
     id: 1,
     title: 'Multi-session, multi-task neural decoding from distinct cell-types and brain regions',
-    authors: 'M. Azabou, R. X. Tan, V. Arora, J. J. Knight, E. L. Dyer, B. Richards',
-    venue: 'Neural Information Processing Systems (NeurIPS)',
+    authors: 'M. Azabou, K. X. Pan, V. Arora, I. J. Knight, E. L. Dyer, B. Richards',
+    venue: 'Accepted at Neural Information Processing Systems (NeurIPS)',
     year: 2024,
     links: {
       paper: '#'
@@ -30,7 +30,7 @@ const publicationsData: Publication[] = [
     id: 2,
     title: 'A Unified, Scalable Framework for Neural Population Decoding',
     authors: 'M. Azabou, V. Arora, V. Ganesh, X. Mao, S. Nachimuthu, M. Mendelson, B. Richards, M. Perich, G. Lajoie, E. L. Dyer',
-    venue: 'Neural Information Processing Systems (NeurIPS)',
+    venue: 'Presented at Neural Information Processing Systems (NeurIPS)',
     year: 2023,
     links: {
       paper: '#',
@@ -62,35 +62,37 @@ const Publications: React.FC = () => {
             <div className="publications__content">
               <h3 className="publications__title">{publication.title}</h3>
               <div className="publications__authors">{publication.authors}</div>
-              <div className="publications__venue">
-                Accepted at {publication.venue}, {publication.year}
-              </div>
-              
-              <div className="publications__links">
-                {publication.links.paper && (
-                  <a href={publication.links.paper} target="_blank" rel="noopener noreferrer">
-                    <FaFilePdf /> Paper
-                  </a>
-                )}
-                
-                {publication.links.project && (
-                  <a href={publication.links.project} target="_blank" rel="noopener noreferrer">
-                    <FaExternalLinkAlt /> Project Page
-                  </a>
-                )}
-                
-                {publication.links.poster && (
-                  <a href={publication.links.poster} target="_blank" rel="noopener noreferrer">
-                    <FaImage /> Poster
-                  </a>
-                )}
-                
-                {publication.links.openReview && (
-                  <a href={publication.links.openReview} target="_blank" rel="noopener noreferrer">
-                    <FaExternalLinkAlt /> OpenReview
-                  </a>
-                )}
-              </div>
+                <div className="publications__details">
+                    <div className="publications__details__venue">
+                        {publication.venue}, {publication.year}
+                    </div>
+                    
+                    <div className="publications__details__links">
+                        {publication.links.paper && (
+                        <a href={publication.links.paper} target="_blank" rel="noopener noreferrer">
+                            <FaFilePdf /> Paper
+                        </a>
+                        )}
+                        
+                        {publication.links.project && (
+                        <a href={publication.links.project} target="_blank" rel="noopener noreferrer">
+                            <FaExternalLinkAlt /> Project Page
+                        </a>
+                        )}
+                        
+                        {publication.links.poster && (
+                        <a href={publication.links.poster} target="_blank" rel="noopener noreferrer">
+                            <FaImage /> Poster
+                        </a>
+                        )}
+                        
+                        {publication.links.openReview && (
+                        <a href={publication.links.openReview} target="_blank" rel="noopener noreferrer">
+                            <FaExternalLinkAlt /> OpenReview
+                        </a>
+                        )}
+                    </div>
+                </div>
             </div>
           </div>
         ))}
