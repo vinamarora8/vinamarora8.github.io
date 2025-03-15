a21b11c34365c51774e390727a40425a6b0bc88e
Vinam Arora <vinam@posteo.de>
2025-03-15 00:28:16 -0400

    Cleanup publications

--- src/components/Publications.tsx
+++ src/components/Publications.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { FaFilePdf, FaImage, FaExternalLinkAlt } from 'react-icons/fa';
 
 interface Publication {
-  id: number;
   title: string;
   authors: string[];
   venue: string;
@@ -17,7 +16,6 @@ interface Publication {
 
 const publicationsData: Publication[] = [
   {
-    id: 1,
     title: 'Multi-session, multi-task neural decoding from distinct cell-types and brain regions',
     authors: ['M. Azabou', 'K. X. Pan', 'V. Arora', 'I. J. Knight', 'E. L. Dyer', 'B. Richards'],
     venue: 'Accepted at Neural Information Processing Systems (NeurIPS)',
@@ -27,7 +25,15 @@ const publicationsData: Publication[] = [
     }
   },
   {
-    id: 2,
+    title: 'GraphFM: A Scalable Framework For Multi-Graph Pretraining',
+    authors: ['Divyansha L.', 'M. Azabou', 'V. Arora', 'E. L. Dyer'],
+    venue: 'arXiv Preprint',
+    year: 2024,
+    links: {
+      paper: 'https://arxiv.org/abs/2407.11907'
+    }
+  },
+  {
     title: 'A Unified, Scalable Framework for Neural Population Decoding',
     authors: ['M. Azabou', 'V. Arora', 'V. Ganesh', 'X. Mao', 'S. Nachimuthu', 'M. Mendelson', 'B. Richards', 'M. Perich', 'G. Lajoie', 'E. L. Dyer'],
     venue: 'Presented at Neural Information Processing Systems (NeurIPS)',
@@ -39,16 +45,6 @@ const publicationsData: Publication[] = [
       openReview: 'https://openreview.net/forum?id=sw2Y0sirtM'
     }
   },
-  {
-    id: 3,
-    title: 'GraphFM: A Scalable Framework For Multi-Graph Pretraining',
-    authors: ['Divyansha L.', 'M. Azabou', 'V. Arora', 'E. L. Dyer'],
-    venue: 'arXiv Preprint',
-    year: 2024,
-    links: {
-      paper: 'https://arxiv.org/abs/2407.11907'
-    }
-  }
 ];
 
 const myName: string = "V. Arora";
@@ -59,8 +55,8 @@ const Publications: React.FC = () => {
       <h2 className="section-title">My Publications</h2>
       
       <div className="publications__list">
-        {publicationsData.map((publication) => (
-          <div key={publication.id} className="publications__item">
+        {publicationsData.map((publication, index) => (
+          <div key={index} className="publications__item">
             <div className="publications__content">
               <h3 className="publications__title">{publication.title}</h3>
               <div className="publications__authors">
