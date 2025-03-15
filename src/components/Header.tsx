2ac51062299615db9eecf76f47dd3ee47ca9e176
Vinam Arora <vinam@posteo.de>
2025-03-15 00:39:26 -0400

    Header react-textfit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Textfit } from 'react-textfit';
 
 const Header: React.FC = () => {
   return (
       <div className="header">
-          <h1 className="header__title">Vinam Arora</h1>
+          <h1 className="header__title">
+            <Textfit 
+              mode="single" 
+              max={50}  // Maximum font size
+              min={24}  // Minimum font size for readability
+            >
+              Vinam Arora
+            </Textfit>
+          </h1>
+          <div className="header__nav"></div>
       </div>
   )
 }
