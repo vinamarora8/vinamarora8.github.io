0bf82e805b436bae1a7f9d084d343fa6c1c739d2
Vinam Arora <vinam@posteo.de>
2025-03-15 23:48:25 -0400

    Basic career path setup

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -1,7 +1,53 @@
 import React from "react";
 
 const Career: React.FC = () => {
-    return <div>sdfds</div>
+    return (
+        <div className="career-container">
+            <div className="career-list">
+                <CareerItem
+                    logo="none"
+                    title="Ph.D. in Machine Learning"
+                    venue="Georgia Institute of Technology, GA, USA"
+                    timeline="2024 - ongoing"
+                />
+            </div>
+        </div>
+    )
 }
 
 export default Career;
+
+interface CareerItemProps {
+    logo: string;
+    title: string;
+    venue: string;
+    timeline: string;
+    description?: string;
+}
+
+const CareerItem: React.FC<CareerItemProps> = ({
+    logo,
+    title,
+    venue,
+    timeline,
+    description,
+}) => {
+    return (
+        <div className="career-item">
+            <div className="career-item__timeline">
+                {timeline}
+            </div>
+            <div className="career-item__bar-container">
+                <div className="career-item__logo"/>
+                <div className="career-item__bar" />
+            </div>
+            <div className="career-item__text">
+                <h3>{title}</h3>
+                <h4>{venue}</h4>
+                {description && <p>{description}</p>}
+            </div>
+
+        </div>
+
+    )
+}
