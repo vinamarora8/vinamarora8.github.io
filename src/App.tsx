f58016a78f41c73dd21fdad870a4f5c9ee27d573
Vinam Arora <vinam@posteo.de>
2025-03-16 04:38:24 -0400

    Support router in gh-pages (single page website problem)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Profile from './components/Profile';
 import Publications from './components/Publications';
