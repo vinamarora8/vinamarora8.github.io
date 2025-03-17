c5f3b6260d7f93de7e457c6ccb08a348df8e9fc0
Vinam Arora <vinam@posteo.de>
2025-03-17 01:31:34 -0400

    formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import yaml from '@rollup/plugin-yaml'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import yaml from '@rollup/plugin-yaml';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), yaml(), tailwindcss()],
   base: '/',
-})
+});
