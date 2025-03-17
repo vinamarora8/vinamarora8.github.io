3fe4bfb3fc9febea21a4cd882aacd11eae59aaf7
Vinam Arora <vinam@posteo.de>
2025-03-17 01:22:32 -0400

    Comments to accordian

--- src/components/Accordian.tsx
+++ src/components/Accordian.tsx
@@ -8,41 +8,65 @@ interface AccordianToggleProps {
 }
 
 const AccordianToggle: React.FC<AccordianToggleProps> = ({ isExpanded, toggleExpanded }) => {
+  const gradientStart = '#ffaec2';
+  const gradientEnd = '#7ec3ff';
+
   return (
-    <div
-      className="group/toggle-button flex cursor-pointer flex-row gap-[9px]"
-      onClick={toggleExpanded}
-    >
-      <div className="relative w-[6px]">
-        <div className="absolute h-full w-full rounded-tl-[3px] rounded-bl-[3px] bg-[#e0e0e0]" />
+    <>
+      {/* Main container */}
+      <div
+        className="group/toggle-button flex cursor-pointer flex-row gap-[9px]"
+        onClick={toggleExpanded}
+      >
+        {/* Left-side border */}
+        <div className="relative w-[6px]">
+          {/* Base div: gray */}
+          <div
+            className={clsx(
+              'absolute h-full w-full',
+              'rounded-tl-[3px] rounded-bl-[3px]',
+              // base color
+              'bg-[#e0e0e0]'
+            )}
+          />
+
+          {/* An overlayed div that becomes visible when mouse hovers on container */}
+          <div
+            className={clsx(
+              'absolute h-full w-full',
+              'rounded-tl-[3px] rounded-bl-[3px]',
+              // Gradient define
+              `bg-gradient-to-b from-[${gradientStart}] to-[${gradientEnd}]`,
+              // Visibility switches
+              'opacity-0 group-hover/full:opacity-100',
+              isExpanded && 'opacity-100',
+              'transition-all duration-100 ease-in'
+            )}
+          />
+        </div>
+
+        {/* Chevron button */}
         <div
           className={clsx(
-            'absolute h-full w-full rounded-tl-[3px] rounded-bl-[3px]',
-            'bg-gradient-to-b from-[#ffaec2] to-[#7ec3ff]',
-            'opacity-0 group-hover/full:opacity-100',
-            'transition-all duration-100 ease-in',
-            isExpanded && 'opacity-100'
+            'mt-[10px]',
+            'text-[#aaa]',
+            // Change color when hovering on the main container
+            `group-hover/toggle-button:text-[${gradientStart}]`,
+            'transition-all duration-100 ease-in'
           )}
-        />
+        >
+          <FaChevronDown
+            style={{
+              transform: isExpanded ? 'rotate(180deg)' : '',
+              width: '9px',
+              height: '9px',
+              transition: 'all 0.2s ease-in-out',
+            }}
+            strokeWidth="30px"
+          />
+        </div>
       </div>
-      <div
-        className={clsx(
-          'mt-[10px]',
-          'text-[#aaa] group-hover/toggle-button:text-[#ffaec2]',
-          'transition-all duration-100 ease-in'
-        )}
-      >
-        <FaChevronDown
-          style={{
-            transform: isExpanded ? 'rotate(180deg)' : '',
-            width: '9px',
-            height: '9px',
-            transition: 'all 0.2s ease-in-out',
-          }}
-          strokeWidth="30px"
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
