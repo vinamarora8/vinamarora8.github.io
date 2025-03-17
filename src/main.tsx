60e3b5cf967a870fc5b8c20f42186b0698e5487c
Vinam Arora <vinam@posteo.de>
2025-03-17 00:46:48 -0400

    Remove normal css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './styles/tailwind.css';
-import './styles/main.scss';
+import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), yaml(), tailwindcss()],
-  // For user site (username.github.io), we don't need a base path
   base: '/',
 })
