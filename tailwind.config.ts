49c12229ca64d2fa32a956571f57109115928c50
Vinam Arora <vinam@posteo.de>
2025-03-16 20:01:50 -0400

    Remove theme from tailwind config ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,23 +5,6 @@ const config: Config = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#333',
-        secondary: '#666',
-        accent: '#4a90e2',
-        background: '#fff',
-      },
-      fontFamily: {
-        serif: ['"Source Serif Pro"', 'serif'],
-      },
-      fontWeight: {
-        light: 300,
-        normal: 400,
-      },
-    },
-  },
   plugins: [],
 };
 
